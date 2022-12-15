ac4e06421d441b907f3ced42b4393bf8ae831e87
Armando Garcia-Jacquier <armando.garcia-jacquier@nytimes.com>
2022-12-16 00:04:52 +0100

    add part two 13

--- 13/main.js
+++ 13/main.js
@@ -10,10 +10,28 @@ if (PART === 'one') {
         const [left, right] = packets.split('\n').map(val => JSON.parse(val))
         // return [...count, comparePackets(left, right)]
         return comparePackets(left, right) ? count + (1 + idx) : count
-    // }, [])
+        // }, [])
     }, 0)
 
     console.log(rightOrder)
+}
+
+if (PART === 'two') {
+    const TwoDiv = [[2]]
+    const SixDiv = [[6]]
+
+    const packetList = puzzle.reduce((flattened, packets, idx) => {
+        const [left, right] = packets.split('\n').map(val => JSON.parse(val))
+        return [...flattened, left, right]
+    }, [TwoDiv, SixDiv])
+
+    const sorted = packetList.sort(sortPackets)
+    // console.log(sorted)
+    const productDivIndexes = sorted.reduce((prod, curr, idx) => {
+        if (curr === TwoDiv || curr === SixDiv) return prod * (idx + 1)
+        return prod
+    }, 1)
+    console.log(productDivIndexes)
 }
 
 function comparePackets(left, right) {
@@ -44,4 +62,27 @@ function comparePackets(left, right) {
     // if loop completed and the isInOrder is null either values where equal or no comparable values were present
     // ensure the left was smaller or equal in length to right
     return left.length < right.length ? true : null
+}
+
+function sortPackets(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        const diff = a - b
+        return diff === 0 ? null : diff
+    }
+
+    if (Array.isArray(a) && !Array.isArray(b)) {
+        return sortPackets(a, [b])
+    }
+
+    if (!Array.isArray(a) && Array.isArray(b)) {
+        return sortPackets([a], b)
+    }
+
+    for (let i = 0; i < a.length; i++) {
+        if (b[i] === undefined) return 1
+
+        const order = sortPackets(a[i], b[i])
+        if (order !== null) return order
+    }
+    return a.length - b.length
 }
